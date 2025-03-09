0f6247e05a556e9af0420d468564be5d918630c3
b_iskandarov <b.iskandarov@uzinfocom.uz>
2025-03-10 00:21:53 +0500

    chore: migrate to github

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto mt-10 flex flex-col items-center gap-10 px-2 text-center">
+      <p className="text-lg">10K+ students trust Study Fox</p>
+      <h1 className="w-max text-8xl font-light">
+        The main heading <br /> goes here
+      </h1>
+      <p className="text-xl">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit{" "}
+        veritatis pariatur deleniti
+        <br /> necessitatibus ipsum quaerat in, hic reiciendis eius iste.
+      </p>
+    </div>
+  );
+}
---
+++ src/shared/ui/header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="container mx-auto flex w-full items-center p-4">
+      <h2 className="font-mono text-2xl">
+        Study <span className="text-orange-500">Fox</span>
+      </h2>
+    </div>
+  );
+}
+
+export default Header;
