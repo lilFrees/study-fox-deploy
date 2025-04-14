9ba71cb1100f823e0b2da8674e7e87028afc80ed
Lil_Frees <lil.trader@mail.ru>
2025-04-14 23:36:09 +0500

    fix: remove logs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,5 @@ import { ReactNode } from "react";
 import "./globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  console.log(process.env.FIREBASE_API_KEY, "asd");
-
   return children;
 }
